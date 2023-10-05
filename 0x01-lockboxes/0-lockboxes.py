1107959898f7420936886bdf2a24d976113b8d6d
FourtyThree43 <shaqmwa@outlook.com>
2023-10-05 19:01:23 +0300

    REALLY FUCKING FIXED

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,42 +1,34 @@
 #!/usr/bin/python3
-""" Module that determines if all the boxes can be opened"""
-# from typing import List
-# from collections import deque
-
-# def canUnlockAll(boxes: List[List[int]]) -> bool:
-#     num_boxes = len(boxes)
-#     unlocked_boxes = set()
-#     keys_seen = set()
-
-#     queue = deque([0])
-
-#     while queue:
-#         box = queue.popleft()
-#         unlocked_boxes.add(box)
-
-#         for key in boxes[box]:
-#             keys_seen.add(key)
-
-#             if key not in unlocked_boxes and key < num_boxes:
-#                 queue.append(key)
-
-#     return len(unlocked_boxes) == num_boxes
+"""
+Module for the canUnlockAll function.
+"""
 
 
 def canUnlockAll(boxes):
-    # Initialize a list to keep track of visited boxes
-    visited = [False] * len(boxes)
+    """
+    Determines if all the boxes can be opened.
+
+    Args:
+        boxes (list of lists): A list of lists representing the boxes and their keys.
+
+    Returns:
+        bool: True if all boxes can be opened, False otherwise.
+    """
+
+    # Initialize a set to keep track of visited boxes and keys seen
+    visited = set()
+    keys_seen = set()
 
     # Start with box 0, which is already unlocked
     stack = [0]
-    visited[0] = True
+    visited.add(0)
 
     while stack:
         current_box = stack.pop()
         for key in boxes[current_box]:
-            if not visited[key]:
+            if key not in visited:
                 stack.append(key)
-                visited[key] = True
+                visited.add(key)
+            keys_seen.add(key)
 
-    # If all boxes are visited, return True; otherwise, return False
-    return all(visited)
+    return len(visited) == len(boxes) and len(keys_seen) == len(boxes)
