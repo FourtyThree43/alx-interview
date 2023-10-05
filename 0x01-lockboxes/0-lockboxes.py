236796f334a7abf2939265451c67c30d63cd94fe
FourtyThree43 <shaqmwa@outlook.com>
2023-10-05 18:56:22 +0300

    lots and lots of changes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,45 +1,9 @@
 #!/usr/bin/python3
 """ Module that determines if all the boxes can be opened"""
-from typing import List
-from collections import deque
+# from typing import List
+# from collections import deque
 
-
-def canUnlockAll(boxes: List[List[int]]) -> bool:
-    """
-    Determine if all boxes can be opened.
-
-    Args:
-    boxes (List[List[int]]): A list of lists where each inner list represents a
-                             box and contains integers representing keys.
-
-    Returns:
-    bool: True if all boxes can be opened, else False.
-
-    DFS Implementation (using Stack):
-        - Time Complexity: O(n), where n is the total number of keys across all
-          boxes.
-        - Space Complexity: O(n), where n is the total num of keys across all
-          boxes.
-        - Data Structure Used: Stack (represented by a set in Python)
-    """
-    if not boxes or len(boxes) == 0:
-        return False
-    if len(boxes) == 1:
-        return True
-
-    opened_boxes = set([0])
-    keys_found = set([key for key in boxes[0]])
-
-    while keys_found:
-        new_key = keys_found.pop()
-        if new_key not in opened_boxes:
-            opened_boxes.add(new_key)
-            keys_found.update(boxes[new_key])
-
-    return len(opened_boxes) == len(boxes)
-
-
-# def canUnlockAll(boxes):
+# def canUnlockAll(boxes: List[List[int]]) -> bool:
 #     num_boxes = len(boxes)
 #     unlocked_boxes = set()
 #     keys_seen = set()
@@ -57,3 +21,22 @@ def canUnlockAll(boxes: List[List[int]]) -> bool:
 #                 queue.append(key)
 
 #     return len(unlocked_boxes) == num_boxes
+
+
+def canUnlockAll(boxes):
+    # Initialize a list to keep track of visited boxes
+    visited = [False] * len(boxes)
+
+    # Start with box 0, which is already unlocked
+    stack = [0]
+    visited[0] = True
+
+    while stack:
+        current_box = stack.pop()
+        for key in boxes[current_box]:
+            if not visited[key]:
+                stack.append(key)
+                visited[key] = True
+
+    # If all boxes are visited, return True; otherwise, return False
+    return all(visited)
