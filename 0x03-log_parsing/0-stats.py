d532b155f1120a1cb548f2568a172c1a7aafd2d1
FourtyThree43 <shaqmwa@outlook.com>
2023-10-21 16:27:01 +0300

    improved function

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,50 +1,86 @@
 #!/usr/bin/python3
-"""
-log parsing
-"""
+"""Module for parsing logs."""
 
-import sys
 import re
+import sys
+from collections import Counter
+from typing import Tuple, Optional
 
 
-def output(log: dict) -> None:
+def parse_log(log: str) -> Tuple[Optional[str], Optional[str]]:
     """
-    helper function to display stats
+    Parse a log entry, extracting the status code and file size.
+
+    Args:
+        log (str): A string representing a single log entry.
+
+    Returns:
+        A tuple containing the status code and file size.
+        If the log entry does not match the expected format, returns None, None
     """
-    print("File size: {}".format(log["file_size"]))
-    for code in sorted(log["code_frequency"]):
-        if log["code_frequency"][code]:
-            print("{}: {}".format(code, log["code_frequency"][code]))
+    parts = (
+        r"\s*(?P<ip>\S+)\s*",
+        r"- \[",
+        r"(?P<date>[^\]]+)",
+        r'\] "(?P<method>GET) (?P<res>/projects/260) (?P<proto>HTTP/1\.1)" ',
+        r"(?P<status_code>\d+)",
+        r" ",
+        r"(?P<file_size>\d+)",
+    )
+
+    log_fmt = "{}{}{}{}{}{}{}\\s*".format(parts[0], parts[1], parts[2],
+                                          parts[3], parts[4], parts[5],
+                                          parts[6])
+
+    match = re.fullmatch(log_fmt, log.strip())
+    if match:
+        status = match.group("status_code")
+        size = match.group("file_size")
+        if status in {"200", "301", "400", "401", "403", "404", "405", "500"}:
+            return status, size
+    return None, None
+
+
+def process_logs() -> None:
+    """
+    Process logs from standard input.
+
+    Reads logs line by line, parses each log to extract the status code and
+    file size, and keeps a running total of the file size and a count
+    of each status code.
+
+    Prints the statistics every 10 lines and when
+    an EOFError or KeyboardInterrupt exception is raised.
+    """
+    status_counter, size_counter = Counter(), Counter()
+    try:
+        for idx, log in enumerate(sys.stdin, start=1):
+            status, size = parse_log(log)
+            if status and size:
+                status_counter[status] += 1
+                size_counter["size"] += int(size)
+            if idx % 10 == 0:
+                print_stats(status_counter, size_counter)
+    except (KeyboardInterrupt, EOFError):
+        print_stats(status_counter, size_counter)
+        sys.exit(0)
+
+
+def print_stats(status_counter: Counter, size_counter: Counter) -> None:
+    """
+    Print statistics about the processed logs.
+
+    Args:
+        status_counter (Counter): A counter of HTTP status codes.
+        size_counter (Counter): A counter of file sizes.
+
+    Prints the total file size and the count of each status code
+    in ascending order.
+    """
+    print(f"\nFile size: {size_counter['size']}")
+    for status in sorted(status_counter):
+        print(f"{status}: {status_counter[status]}")
 
 
 if __name__ == "__main__":
-    regex = re.compile(
-        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - \[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d+\] "GET /projects/260 HTTP/1.1" (.{3}) (\d+)'
-    )  # nopep8
-
-    line_count = 0
-    log = {}
-    log["file_size"] = 0
-    log["code_frequency"] = {
-        str(code): 0
-        for code in [200, 301, 400, 401, 403, 404, 405, 500]
-    }
-
-    try:
-        for line in sys.stdin:
-            line = line.strip()
-            match = regex.fullmatch(line)
-            if match:
-                line_count += 1
-                code = match.group(1)
-                file_size = int(match.group(2))
-
-                log["file_size"] += file_size
-
-                if code.isdecimal():
-                    log["code_frequency"][code] += 1
-
-                if line_count % 10 == 0:
-                    output(log)
-    finally:
-        output(log)
+    process_logs()
