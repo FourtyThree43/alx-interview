d2982423b9c09c372799f597d1098d92f922c255
FourtyThree43 <shaqmwa@outlook.com>
2023-10-21 15:17:06 +0300

    I'm guessing this may start causing us problems either soon or never.

--- 0x03-log_parsing/1-stats.go
+++ 0x03-log_parsing/1-stats.go
@@ -1,20 +1,82 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"os/signal"
 	"regexp"
+	"sort"
+	"strconv"
+	"syscall"
 )
 
-// Module for log parsing script
-// This module will parse the log file and return the following stats:
-// - Average size in bytes of a response
-// - Number of requests by HTTP method
-// - Top 10 resources by number of requests
-// - Number of requests by response code
-// - Top 10 IPs by number of requests
+var (
+	totalSize int
+	statusMap = make(map[int]int)
+)
+
+// printStats prints the total file size and the count of each status code.
+func printStats() {
+	fmt.Printf("Total file size: %d\n", totalSize)
+	var keys []int
+	for k := range statusMap {
+		keys = append(keys, k)
+	}
+	sort.Ints(keys)
+	for _, k := range keys {
+		fmt.Printf("%d: %d\n", k, statusMap[k])
+	}
+}
+
+
+// main reads from stdin, parses each line using a regular expression, and updates the total file size and the status code count.
+// After every 10 lines or when a keyboard interruption (CTRL + C) is detected, it invokes printStats to print the statistics.
+// If there's an error reading from stdin, it prints that error to stderr.
 func main() {
-	// Read log from stdin
-	// Parse log
-	// processLog(log)
-	// Print stats
+	scanner := bufio.NewScanner(os.Stdin)
+	lines := 0
+
+	c := make(chan os.Signal)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-c
+		printStats()
+		os.Exit(1)
+	}()
+
+	for scanner.Scan() {
+		lines++
+		line := scanner.Text()
+
+        re := regexp.MustCompile(`\s*(?P<ip>\S+)\s*- \[(?P<date>[^\]]+)\] "(?P<method>GET) (?P<res>/projects/260) (?P<proto>HTTP/1\.1)" (?P<status_code>\d+) (?P<file_size>\d+)\s*`)
+        match := re.FindStringSubmatch(line)
+
+        if len(match) == 0 {
+            continue
+        }
+
+        size, err := strconv.Atoi(match[7])
+        if err != nil {
+            continue
+        }
+        totalSize += size
+
+        status, err := strconv.Atoi(match[6])
+        if err != nil {
+            continue
+        }
+
+        if status == 200 || status == 301 || status == 400 || status == 401 || status == 403 || status == 404 || status == 405 || status == 500 {
+            statusMap[status]++
+        }
+
+        if lines%10 == 0 {
+            printStats()
+        }
+    }
+
+    if err := scanner.Err(); err != nil {
+        fmt.Fprintln(os.Stderr, "reading standard input:", err)
+    }
 }
