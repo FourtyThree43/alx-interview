c6acd6a5130c43809c5dd1921f7317dbf61fdecd
FourtyThree43 <shaqmwa@outlook.com>
2023-10-26 23:29:46 +0300

    Fixed compilation errors

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -8,6 +8,10 @@ def validUTF8(data: List[int]) -> bool:
     """ Check is a list of integers is valid UTF-8 encoded data
         Return: True if data is valid UTF-8 encoded, else return False
     """
+    if not all(isinstance(i, int) for i in data):
+        return False
+    if not all(i >= 0 for i in data):
+        return False
     try:
         bytes(data).decode('utf-8')
         return True
