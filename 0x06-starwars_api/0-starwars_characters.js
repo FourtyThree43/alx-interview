e44d53b3954569e7f6a5121d32187ca40ea0bd75
FourtyThree43 <shaqmwa@outlook.com>
2023-11-09 21:37:57 +0300

    SEXY RUSSIAN CODES WAITING FOR YOU TO CALL

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,51 +1,50 @@
 #!/usr/bin/node
 const request = require('request');
 
-function fetch(url) {
-	return new Promise((resolve, reject) => {
-		request.get(url, (err, response, body) => {
-			if (err) {
-				reject(err);
-			} else if (response.statusCode === 200) {
-				resolve(body);
-			} else {
-				reject(`Error code: ${response.statusCode}`);
-			}
-		});
-	});
+function fetch (url) {
+  return new Promise((resolve, reject) => {
+    request.get(url, (err, response, body) => {
+      if (err) {
+        reject(err);
+      } else if (response.statusCode === 200) {
+        resolve(body);
+      } else {
+        reject(`Error code: ${response.statusCode}`);
+      }
+    });
+  });
 }
 
-function fetchMovieCharacters(movieID) {
-	const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieID}/`;
+function fetchCharactersRecursively (charData, index) {
+  if (index >= charData.length) {
+    return;
+  }
 
-	fetch(apiUrl).then(body => {
-		const movieData = JSON.parse(body);
-		const charData = movieData.characters;
-		fetchCharactersRecursively(charData, 0);
-	}).catch(err => console.error(err));
+  const characterUrl = charData[index];
+
+  fetch(characterUrl).then(body => {
+    const character = JSON.parse(body);
+    console.log(character.name);
+    fetchCharactersRecursively(charData, index + 1);
+  }).catch(err => {
+    console.error(err);
+    fetchCharactersRecursively(charData, index + 1);
+  });
 }
 
-function fetchCharactersRecursively(charData, index) {
-	if (index >= charData.length) {
-		return;
-	}
+function fetchMovieCharacters (movieID) {
+  const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieID}/`;
 
-	const characterUrl = charData[index];
-
-	fetch(characterUrl).then(body => {
-		const character = JSON.parse(body);
-		console.log(character.name);
-		fetchCharactersRecursively(charData, index + 1);
-	}).catch(err => {
-		console.error(err);
-		fetchCharactersRecursively(charData, index + 1);
-	});
+  fetch(apiUrl).then(body => {
+    const movieData = JSON.parse(body);
+    const charData = movieData.characters;
+    fetchCharactersRecursively(charData, 0);
+  }).catch(err => console.error(err));
 }
-
 
 const movieID = process.argv[2];
 if (!movieID) {
-	console.log('./0-starwars_characters.js <movie_ID>');
+  console.log('./0-starwars_characters.js <movie_ID>');
 } else {
-	fetchMovieCharacters(movieID);
+  fetchMovieCharacters(movieID);
 }
