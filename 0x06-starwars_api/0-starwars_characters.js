48d459547fa12ac85b9d2cd62d55367de57f8393
FourtyThree43 <shaqmwa@outlook.com>
2023-11-09 21:06:30 +0300

    work in progress

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,51 +1,29 @@
 #!/usr/bin/node
 const request = require('request');
 
-function fetch(url) {
-	return new Promise((resolve, reject) => {
-		request.get(url, (err, response, body) => {
-			if (err) {
-				reject(err);
-			} else if (response.statusCode === 200) {
-				resolve(body);
-			} else {
-				reject(`Error code: ${response.statusCode}`);
-			}
-		});
-	});
-}
+const movieID = process.argv[2];
+const apiUrl = 'https://swapi-api.hbtn.io/api/films/';
 
-function fetchMovieCharacters(movieID) {
-	const apiUrl = `https://swapi-api.hbtn.io/api/films/${movieID}/`;
-
-	fetch(apiUrl).then(body => {
+request.get(apiUrl + movieID, (err, response, body) => {
+	if (err) {
+		console.error(err);
+	} else if (response.statusCode === 200) {
 		const movieData = JSON.parse(body);
 		const charData = movieData.characters;
-		fetchCharactersRecursively(charData, 0);
-	}).catch(err => console.error(err));
-}
 
-function fetchCharactersRecursively(charData, index) {
-	if (index >= charData.length) {
-		return;
+		for (const char of charData) {
+			request.get(char, (err, response, body) => {
+				if (err) {
+					console.error(err);
+				} else if (response.statusCode === 200) {
+					const character = JSON.parse(body);
+					console.log(character.name);
+				} else {
+					console.log(`Error code: ${response.statusCode}`);
+				}
+			});
+		}
+	} else {
+		console.log(`Error code: ${response.statusCode}`);
 	}
-
-	const characterUrl = charData[index];
-
-	fetch(characterUrl).then(body => {
-		const character = JSON.parse(body);
-		console.log(character.name);
-		fetchCharactersRecursively(charData, index + 1);
-	}).catch(err => {
-		console.error(err);
-		fetchCharactersRecursively(charData, index + 1);
-	});
-}
-
-
-const movieID = process.argv[2];
-if (!movieID) {
-	console.log('./0-starwars_characters.js <movie_ID>');
-} else {
-	fetchMovieCharacters(movieID);
-}
+});
