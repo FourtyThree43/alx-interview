84f4d96257a6a24de932a7eeb8433bf85b011083
FourtyThree43 <shaqmwa@outlook.com>
2023-11-09 22:00:51 +0300

    This commit is a lie

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,29 +1,46 @@
 #!/usr/bin/node
 const request = require('request');
 
-const movieID = process.argv[2];
-const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieID}/`;
+function fetch (url) {
+  return new Promise((resolve, reject) => {
+    request.get(url, (err, response, body) => {
+      if (err) {
+        reject(err);
+      } else if (response.statusCode === 200) {
+        resolve(body);
+      } else {
+        reject(new Error(`Request failed. Status code: ${response.statusCode}`));
+      }
+    });
+  });
+}
 
-request.get(apiUrl, (err, response, body) => {
-  if (err) {
+function fetchCharactersRecursively (charData, index) {
+  if (index >= charData.length) {
+    return;
+  }
+
+  const characterUrl = charData[index];
+
+  fetch(characterUrl).then(body => {
+    const character = JSON.parse(body);
+    console.log(character.name);
+    fetchCharactersRecursively(charData, index + 1);
+  }).catch(err => {
     console.error(err);
-  } else if (response.statusCode === 200) {
+    fetchCharactersRecursively(charData, index + 1);
+  });
+}
+
+function fetchMovieCharacters (movieID) {
+  const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieID}/`;
+
+  fetch(apiUrl).then(body => {
     const movieData = JSON.parse(body);
     const charData = movieData.characters;
+    fetchCharactersRecursively(charData, 0);
+  }).catch(err => console.error(err));
+}
 
-    charData.forEach((charUrl) => {
-      request.get(charUrl, (err, response, body) => {
-        if (err) {
-          console.error(err);
-        } else if (response.statusCode === 200) {
-          const character = JSON.parse(body);
-          console.log(character.name);
-        } else {
-          console.log(`Error code: ${response.statusCode}`);
-        }
-      });
-    });
-  } else {
-    console.log(`Error code: ${response.statusCode}`);
-  }
-});
+const movieID = process.argv[2];
+fetchMovieCharacters(movieID);
